f3d84ce91be4b9c40bcd33f976c9280a7055ec3f
Soundariya-Babu <soundariyababu01@gmail.com>
2023-09-09 11:22:23 +0000

    fun

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AdminComponent } from './admin/admin.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminComponent
+    LoginComponent
   ],
   imports: [
     BrowserModule,
